f15a8736b0846d9db455571dee01d6d7a97e4f70
mhkjahromi <m.h.k.jahromi@gmail.com>
2019-12-21 23:07:20 +0330

    gemini: [TMP] Import Keyhandler and fix buttun backlight
    kang from MisterJaworro device tree
    
    Signed-off-by: mhkjahromi <m.h.k.jahromi@gmail.com>

--- device.mk
+++ device.mk
@@ -27,8 +27,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-extended
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-extended/frameworks
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
